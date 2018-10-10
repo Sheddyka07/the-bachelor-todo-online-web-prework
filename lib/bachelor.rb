b39b8f207828628a6cc8eef1a7e463a223eae8a9
Kelly Sheddy <jungaka07@gmail.com>
2018-10-10 23:02:03 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -2,7 +2,7 @@ def get_first_name_of_season_winner(data, season)
   data.each do |season_key, contestant_values|
     if season_key == season
       contestant_values.each do |status|
-        
+        if status["status"] == 
   
   season.each do |season_key, info|
     info.each do |info_key, data|
