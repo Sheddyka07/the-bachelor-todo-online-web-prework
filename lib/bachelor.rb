1eb5df3a6a9a634bdb1551df7ab642072e14ed1a
Kelly Sheddy <jungaka07@gmail.com>
2018-10-10 23:22:33 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -51,7 +51,7 @@ def get_average_age_for_season(data, season)
   data.each do |season, contestants_data|
     contestants_data.each do |age_search|
       if age_search["age"] == age 
-        age.to_f << array[]
+        array << age.to_f 
       end
     end
   end
