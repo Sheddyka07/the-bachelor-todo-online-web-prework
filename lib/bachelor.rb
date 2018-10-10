7583f71d73f38def16373de774e6340227bddae9
Kelly Sheddy <jungaka07@gmail.com>
2018-10-10 22:54:10 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -2,8 +2,10 @@ def get_first_name_of_season_winner(data, season)
   season.each do |season_key, info|
     info.each do |info_key, data|
       if info_key == "status" && data == "Winner"
-        winner = 
-        
+        winner = :name
+      end
+    end
+    winner    
 end
 
 def get_contestant_name(data, occupation)
