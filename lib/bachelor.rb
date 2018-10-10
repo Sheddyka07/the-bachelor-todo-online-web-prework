33752fd8807db55b7a624b8565b92c996f1bfe39
Kelly Sheddy <jungaka07@gmail.com>
2018-10-10 23:17:07 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -36,7 +36,13 @@ def count_contestants_by_hometown(data, hometown)
 end	
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |season, contestants_data|
+    contestants_data.each do |hometown_search|
+      if hometown_search["hometown"] == hometown
+        return hometown_search["name"]
+      end
+    end
+  end
 end
 
 def get_average_age_for_season(data, season)
