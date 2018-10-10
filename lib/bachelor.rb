b68397a486045181b9e1de8972e9b40c2950b7af
Kelly Sheddy <jungaka07@gmail.com>
2018-10-10 23:29:00 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -48,7 +48,7 @@ end
 def get_average_age_for_season(data, season)
   arr = []   
   ans = ""   
-   data[season].each do | season_key, contestant_data|
+   data[season].each do |season_key, contestant_data|
       arr << season_key["age"].to_f
    end  
   arr_sum = arr.inject(0){|sum,x| sum + x }
